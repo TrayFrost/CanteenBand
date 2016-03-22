f796f20291f5423f61256d4ad33da7a25b8db5ed
TrayFrost <wwwtrayfrost@gmail.com>
2016-03-22 03:24:29 +0300

    Publish

---
+++ CANTEEN.bat
@@ -0,0 +1,28 @@
+@ECHO off
+
+CHCP 65001 > NUL
+CD /d "%~dp0"
+
+IF EXIST %SYSTEMROOT%\py.exe (
+    CMD /k py.exe -3.5 run.py
+    EXIT
+)
+
+rem git --version > NUL 2>&1
+rem IF %ERRORLEVEL% NEQ 0 GOTO nogit
+
+python --version > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 GOTO nopython
+
+CMD /k python run.py
+GOTO end
+
+rem :nogit
+rem ECHO ERROR: Git has either not been installed or not added to your PATH.
+rem GOTO end
+
+:nopython
+ECHO ERROR: Python has either not been installed or not added to your PATH.
+
+:end
+PAUSE
